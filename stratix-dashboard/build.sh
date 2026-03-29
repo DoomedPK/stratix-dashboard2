60d8e72244b9cf40fb7ffb36607db214f8ed37dc
DoomedPK <lonelyprodigy18@gmail.com>
2026-03-28 23:35:31 -0400

    build.sh

--- stratix-dashboard/build.sh
+++ stratix-dashboard/build.sh
@@ -2,10 +2,14 @@
 # exit on error
 set -o errexit
 
-# Install dependencies from the correct folder
+# Install dependencies
 pip install -r stratix-dashboard/requirements.txt
 
-# Run Django commands from within the project folder
+# Run Django commands
 cd stratix-dashboard
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Automatically create the superuser on Supabase!
+# (The || true prevents build crashes on future deploys if the user already exists)
+python create_admin.py || true
