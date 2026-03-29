3f0e0047fe90652cc7f927374729c641459302e5
DoomedPK <lonelyprodigy18@gmail.com>
2026-03-29 17:06:22 -0400

    Update urls.py

--- stratix-dashboard/core/urls.py
+++ stratix-dashboard/core/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 from reports.views import custom_logout
 
 urlpatterns = [
@@ -9,5 +10,7 @@ urlpatterns = [
     path('', include('reports.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('logout/', custom_logout, name='logout'),
+    
+    # This securely serves images on Render's disk even when DEBUG=False
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
