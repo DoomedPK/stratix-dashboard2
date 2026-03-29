4e90533427f7b83304a519bb98c09b2ca53724a4
DoomedPK <lonelyprodigy18@gmail.com>
2026-03-29 18:19:44 -0400

    Update build.sh

--- stratix-dashboard/build.sh
+++ stratix-dashboard/build.sh
@@ -10,6 +10,8 @@ cd stratix-dashboard
 python manage.py collectstatic --no-input
 python manage.py migrate
 
+python manage.py makemigrations 
+python manage.py migrate
 # Automatically create the superuser on Supabase!
 # (The || true prevents build crashes on future deploys if the user already exists)
 python create_admin.py || true
