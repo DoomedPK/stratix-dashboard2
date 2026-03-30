cb1182e770de2492a2b7ed7122b4499a90cf2450
DoomedPK <lonelyprodigy18@gmail.com>
2026-03-30 01:49:41 -0400

    Update admin.py

--- stratix-dashboard/reports/admin.py
+++ stratix-dashboard/reports/admin.py
@@ -1,30 +1,26 @@
 from django.contrib import admin
-from django.contrib.auth.models import User, Group, Permission
+from django.contrib.auth.models import User, Group
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.admin import GroupAdmin as BaseGroupAdmin
+from unfold.admin import ModelAdmin, TabularInline
+from unfold.forms import AdminPasswordChangeForm, UserChangeForm, UserCreationForm
 from .models import Client, Project, UserProfile, Site, Report, SitePhoto, ActivityAlert, SiteIssue
 from django.utils.html import format_html
 
-admin.site.site_header = "Stratix Command Center"
-admin.site.site_title = "Stratix Admin Portal"
-admin.site.index_title = "Global Database Administration"
-
 admin.site.unregister(Group)
 @admin.register(Group)
-class CustomGroupAdmin(BaseGroupAdmin):
+class CustomGroupAdmin(BaseGroupAdmin, ModelAdmin):
     search_fields = ('name',)
-
-@admin.register(Permission)
-class PermissionAdmin(admin.ModelAdmin):
-    search_fields = ('name', 'codename')
-    list_display = ('name', 'content_type', 'codename')
 
 admin.site.unregister(User)
 @admin.register(User)
-class CustomUserAdmin(BaseUserAdmin):
+class CustomUserAdmin(BaseUserAdmin, ModelAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+    change_password_form = AdminPasswordChangeForm
     autocomplete_fields = ['groups', 'user_permissions']
 
-class SitePhotoInline(admin.TabularInline):
+class SitePhotoInline(TabularInline):
     model = SitePhoto
     extra = 0 
     readonly_fields = ['image_preview', 'uploaded_at']
@@ -37,20 +33,20 @@ class SitePhotoInline(admin.TabularInline):
     image_preview.short_description = 'Preview'
 
 @admin.register(Client)
-class ClientAdmin(admin.ModelAdmin):
+class ClientAdmin(ModelAdmin):
     list_display = ('name',) 
     search_fields = ('name',)
     ordering = ('name',)
 
 @admin.register(Project)
-class ProjectAdmin(admin.ModelAdmin):
+class ProjectAdmin(ModelAdmin):
     list_display = ('name', 'client', 'require_photo_minimums', 'status', 'start_date', 'end_date')
     list_filter = ('status', 'client', 'require_photo_minimums')
     search_fields = ('name',)
     ordering = ('-start_date',)
 
 @admin.register(Site)
-class SiteAdmin(admin.ModelAdmin):
+class SiteAdmin(ModelAdmin):
     list_display = ('site_id', 'site_name', 'project', 'priority')
     list_filter = ('priority', 'project__client', 'project')
     search_fields = ('site_id', 'site_name', 'location')
@@ -58,21 +54,21 @@ class SiteAdmin(admin.ModelAdmin):
     autocomplete_fields = ['assigned_contractors']
 
 @admin.register(Report)
-class ReportAdmin(admin.ModelAdmin):
+class ReportAdmin(ModelAdmin):
     list_display = ('site', 'status', 'submitted_at')
     list_filter = ('status',)
     search_fields = ('site__site_id', 'site__site_name')
     readonly_fields = ('submitted_at',)
 
 @admin.register(UserProfile)
-class UserProfileAdmin(admin.ModelAdmin):
+class UserProfileAdmin(ModelAdmin):
     list_display = ('user', 'role', 'client')
     list_filter = ('role', 'client')
     search_fields = ('user__username', 'user__first_name', 'user__last_name')
     autocomplete_fields = ['user']
 
 @admin.register(SitePhoto)
-class SitePhotoAdmin(admin.ModelAdmin):
+class SitePhotoAdmin(ModelAdmin):
     list_display = ('site', 'category', 'contractor', 'status', 'image_thumbnail', 'uploaded_at')
     list_filter = ('status', 'category', 'contractor')
     search_fields = ('site__site_id', 'contractor_notes', 'qa_feedback')
@@ -85,14 +81,14 @@ class SitePhotoAdmin(admin.ModelAdmin):
     image_thumbnail.short_description = 'Photo'
 
 @admin.register(SiteIssue)
-class SiteIssueAdmin(admin.ModelAdmin):
+class SiteIssueAdmin(ModelAdmin):
     list_display = ('site', 'severity', 'is_resolved', 'reported_by', 'created_at')
     list_filter = ('severity', 'is_resolved')
     search_fields = ('site__site_id', 'description')
     autocomplete_fields = ['site', 'reported_by']
 
 @admin.register(ActivityAlert)
-class ActivityAlertAdmin(admin.ModelAdmin):
+class ActivityAlertAdmin(ModelAdmin):
     list_display = ('site', 'alert_type', 'user', 'timestamp', 'message')
     list_filter = ('alert_type', 'timestamp')
     search_fields = ('site__site_id', 'message', 'user__username')
